c6938c25c30e5add97119677be087d424e8ff16a
ferarcin <93416426+ferarcin@users.noreply.github.com>
2021-11-26 09:26:14 -0500

    Nueva Version

--- src/main/java/com/usa/ciclo3/ciclo3/model/Admin.java
+++ src/main/java/com/usa/ciclo3/ciclo3/model/Admin.java
@@ -1,9 +1,18 @@
 package com.usa.ciclo3.ciclo3.model;
 
 import lombok.Data;
-import javax.persistence.Table
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.criteria.CriteriaBuilder;
 
 @Data
 @Table (name="admin")
 public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
 }
